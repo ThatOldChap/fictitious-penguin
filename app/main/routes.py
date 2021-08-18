40c44d37a0fc017a33eb55b3bab35522c4f1ce48
michael <michaeljchap@gmail.com>
2021-08-17 23:20:25 -0400

    Added user profile route and template

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,19 @@
-from flask import render_template, flash, redirect, url_for, request, current_app, g, jsonify
+from flask import render_template, url_for, request, current_app
 from flask_login import current_user, login_required
 from app import db
 from app.main import bp
+from app.models import User
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
 
     return render_template('index.html', title='Home')
+
+
+@bp.route('/user/<username>')
+@login_required
+def user(username):
+    # Get the profile of the current_user
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
