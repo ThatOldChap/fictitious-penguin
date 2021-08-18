8e46f48ceca3bc5093b2f66dd11bdb0c212f49fc
michael <michaeljchap@gmail.com>
2021-08-17 23:39:25 -0400

    Added profile editing feature

---
+++ app/main/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Length
+from app.models import User
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+
+        # Get the original username as the form is created
+        self.original_username = original_username
+
+    # Customer validator method to ensure the new username is unique
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, url_for, request, current_app
+from flask import render_template, url_for, request, current_app, redirect, flash
 from flask_login import current_user, login_required
 from app import db
 from app.main import bp
 from app.models import User
+from app.main.forms import EditProfileForm
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
@@ -17,3 +18,25 @@ def user(username):
     # Get the profile of the current_user
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+
+    form = EditProfileForm(current_user.username)
+
+    # User has changed their profile information
+    if form.validate_on_submit():
+
+        # Update the database with the user's changes to their profile information
+        current_user.username = form.username.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('main.edit_profile'))
+
+    # User is only visiting their profile
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
