d708f0d7b503aabdf3c3bb99e44c62ca9c01c8b0
michael <michaeljchap@gmail.com>
2021-08-17 22:14:23 -0400

    Added auth files

---
+++ app/auth/routes.py
@@ -0,0 +1,114 @@
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_login import login_user, logout_user, current_user
+from app import db
+from app.auth import bp
+from app.auth.forms import LoginForm, RegistrationForm, \
+    ResetPasswordRequestForm, ResetPasswordForm
+from app.models import User
+from app.auth.email import send_password_reset_email
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+
+    # Check to see if the current user is already registered before proceeding
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    # Check to see if a validated form has been submitted in the POST request 
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        # Checks to ensure the user is valid
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+
+        # Signs in the validated user
+        login_user(user, remember=form.remember_me.data)
+
+        # Redirect to page the user had previously specified
+        # Protects against an attacker inserting a malicious URL into the next argument
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.index')
+        return redirect(next_page)
+
+    return render_template('auth/login.html', title='Sign In', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+
+    # Check to see if the current user is already registered before proceeding
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    # Check to see if a validated form has been submitted in the POST request 
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+        # Add the new registered user into the database
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', title='Register', form=form)
+
+
+@bp.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+
+    # Check to see if the current user is already registered before proceeding
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    # Check to see if a validated form has been submitted in the POST request 
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+
+        # Email the user the password reset if valid
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/reset_password_request.html',
+                           title='Reset Password', form=form)
+
+
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+
+    # Check to see if the current user is already registered before proceeding
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    # Check if the user's reset password token is valid
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('main.index'))
+
+    # Check to see if a validated form has been submitted in the POST request 
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+
+        # Set the new password for the user
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/reset_password.html', form=form)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+from time import time
 from flask import current_app
-from app import db
+from flask_login import UserMixin
+from app import db, login
 from app.utils import ErrorType, TestPointListType
+import jwt
+from hashlib import md5
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class TestPoint(db.Model):
@@ -165,7 +170,7 @@ class Channel(db.Model):
             print(f'Error building TestPoint value list. Only {num_testpoints - num_leftover}/{num_testpoints} added successfully.')
         else:
             print(f'Successfully built TestPoint value list with {num_testpoints} points.')
-            
+
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -229,6 +234,51 @@ class Client(db.Model):
     # Relationships
     projects = db.relationship('Project', backref='client', lazy='dynamic')
 
+
     def __repr__(self):
         return f'<Client: {self.name}>'
 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+
+    def __repr__(self):
+        return f'<User {self.username}: {self.email}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        # Generate a string with hexidecimal digits using the user's email
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=retro&s={size}'
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'], algorithm='HS256'
+        )
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
+
+# Keeps track of the logged in user by storing it Flask's user session
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
